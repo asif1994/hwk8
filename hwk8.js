f8bc2aadc7059e2ee574660c7b49ecf5b18a52dd
Bryan Ho <bryan.ho@utexas.edu>
2015-10-29 22:12:00 -0500

    new changes

--- hwk8.js
+++ hwk8.js
@@ -35,31 +35,72 @@ function assign_img() {
 	}
 }
 
-var checkTable = [[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-function allowDrop(event) {
-    event.preventDefault();
-}
+/*
+ Define variables for the values computed by the grabber event 
+ handler but needed by mover event handler
+*/
+var diffX, diffY, theElement;
 
-function drag(event) {
-    event.dataTransfer.setData("image", event.target.id);
-}
 
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("image");
-    var str = event.currentTarget.id;
-    var i = parseInt(str[1]);
-    var j = parseInt(str[2]);
+// The event handler function for grabbing the word
+function grabber(event) {
 
-    if (checkTable[i][j] == 0){
-      event.currentTarget.appendChild(document.getElementById(data));
-      checkTable[i][j] = 1;
-      document.getElementById("works").value = checkTable;
-    } else {
-      
-      event.currentTarget.replaceChild(document.getElementById(data), document.getElementById(event.currentTarget).id.src);
-      document.getElementById("works").value = event.currentTarget.id;
-    /*event.currentTarget.appendChild(document.getElementById(data));*/
+// Set the global variable for the element to be moved
 
-  }
-}
+  theElement = event.currentTarget;
+
+// Determine the position of the word to be grabbed,
+//  first removing the units from left and top
+
+  var posX = parseInt(theElement.style.left);
+  var posY = parseInt(theElement.style.top);
+
+// Compute the difference between where it is and
+// where the mouse click occurred
+
+  diffX = event.clientX - posX;
+  diffY = event.clientY - posY;
+
+// Now register the event handlers for moving and
+// dropping the word
+
+  document.addEventListener("mousemove", mover, true);
+  document.addEventListener("mouseup", dropper, true);
+
+// Stop propagation of the event and stop any default
+// browser action
+
+  event.stopPropagation();
+  event.preventDefault();
+
+}  //** end of grabber
+
+// *******************************************************
+
+// The event handler function for moving the word
+
+function mover(event) {
+// Compute the new position, add the units, and move the word
+
+  theElement.style.left = (event.clientX - diffX) + "px";
+  theElement.style.top = (event.clientY - diffY) + "px";
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of mover
+
+// *********************************************************
+// The event handler function for dropping the word
+
+function dropper(event) {
+
+// Unregister the event handlers for mouseup and mousemove
+
+  document.removeEventListener("mouseup", dropper, true);
+  document.removeEventListener("mousemove", mover, true);
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of dropper
