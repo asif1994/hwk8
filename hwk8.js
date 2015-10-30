3c6a6a9bda9ba576d257aa1f1f9729525b2fb850
Bryan Ho <bryan.ho@utexas.edu>
2015-10-29 22:19:47 -0500

    new changes 2

--- hwk8.js
+++ hwk8.js
@@ -35,98 +35,39 @@ function assign_img() {
 	}
 }
 
-/*
- Define variables for the values computed by the grabber event 
- handler but needed by mover event handler
-*/
+
 var diffX, diffY, theElement;
 
-
-<<<<<<< HEAD
-// The event handler function for grabbing the word
 function grabber(event) {
 
-// Set the global variable for the element to be moved
-
   theElement = event.currentTarget;
-
-// Determine the position of the word to be grabbed,
-//  first removing the units from left and top
 
   var posX = parseInt(theElement.style.left);
   var posY = parseInt(theElement.style.top);
 
-// Compute the difference between where it is and
-// where the mouse click occurred
-
   diffX = event.clientX - posX;
   diffY = event.clientY - posY;
-
-// Now register the event handlers for moving and
-// dropping the word
 
   document.addEventListener("mousemove", mover, true);
   document.addEventListener("mouseup", dropper, true);
 
-// Stop propagation of the event and stop any default
-// browser action
-
   event.stopPropagation();
   event.preventDefault();
 
-}  //** end of grabber
-
-// *******************************************************
-
-// The event handler function for moving the word
+}
 
 function mover(event) {
-// Compute the new position, add the units, and move the word
 
   theElement.style.left = (event.clientX - diffX) + "px";
   theElement.style.top = (event.clientY - diffY) + "px";
 
-// Prevent propagation of the event
-
   event.stopPropagation();
-}  //** end of mover
-
-// *********************************************************
-// The event handler function for dropping the word
+}
 
 function dropper(event) {
-
-// Unregister the event handlers for mouseup and mousemove
 
   document.removeEventListener("mouseup", dropper, true);
   document.removeEventListener("mousemove", mover, true);
 
-// Prevent propagation of the event
-
   event.stopPropagation();
-}  //** end of dropper
-=======
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("image");
-    var str = event.currentTarget.id;
-    var i = parseInt(str[1]);
-    var j = parseInt(str[2]);
-
-    if (checkTable[i][j] == 0){
-      /*event.currentTarget.appendChild(document.getElementById(data));*/
-      event.currentTarget.src = event.currentTarget.appendChild(document.getElementById(data));
-      checkTable[i][j] = 1;
-      document.getElementById("works").value = "if";
-    } else {
-      var old_img = event.currentTarget.src;
-      event.currentTarget.replaceChild(document.getElementById(data), event.currentTarget.src);
-      
-      document.getElementById("works").value =document.getElementById("d0");
-     document.getElementById("d0") = old_img;
-      
-    /*event.currentTarget.appendChild(document.getElementById(data));*/
-
-  }
 }
->>>>>>> d4e5caf99a946962087ab52c13021e2c327eba5d
