c3f585a9894c709dce60221a20551e89d566763c
asif chowdhury <asif.chowdhury64@gmail.com>
2015-11-20 00:24:19 -0600

    no message

--- hwk12.php
+++ hwk12.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 
 if (isset($_POST["check"])) {
     check();
-} else if (isset($_POST["first_q"])) {
-    first_q();
+} else if (isset($_POST["questions"])) {
+    questions();
 } else {
     login();
 }
@@ -23,7 +23,7 @@ function login(){
     <body>
     <center>
     <h2> Please Login to Continue </h2>
-    <form method = "post" action = "$script">
+    <form method = "post" action = $script>
     <table width = "400px" height="100px">
     <tr>
     <td> User name: </td>
@@ -69,19 +69,10 @@ function check(){
       break;
     }
   }
+
   if ($found){
-      session_start();
-
-      if (!isset($_SESSION["question_num"])){
-	$_SESSION["answer"] = "";
-	$_SESSION["score"] = 0;
-	$_SESSION["question_num"] = 0;
-      }
-
-      $total_questions = 6;
-
       $script = $_SERVER['PHP_SELF'];
-      print <<<TOP
+      print <<<WELCOME
 	<html>
 	<head>
 	<title> Astronomy Quiz </title>
@@ -90,22 +81,15 @@ function check(){
 	<center>
 	<h1> Astronomy Quiz </h1>
 	</center>
-TOP;
-	$score = $_SESSION["score"];
-	$question_num = $_SESSION["question_num"];
-	
-	if ($question_num == 0){
-	  print <<<FIRST
-	  <form method="POST" action="$script">
-	  <p> You will be given $total_questions questions in this quiz. You have 15 minutes to complete this quiz. <br /><br/>
+        <form method="POST" action=$script>
+	  <p> You will be given 6 questions in this quiz. You have 15 minutes to complete this quiz. <br /><br/>
 	      Click below when you are ready to start the quiz. <br>
-	      <input type=submit name="first_q" value="START"/>
+	      <input type=submit name="questions" value="START"/>
 	  </p>
           </form>
 	  </body>
 	  </html>
-FIRST;
-	}
+WELCOME;
   }
   else{
      login();
@@ -117,8 +101,19 @@ FIRST;
 ##----------------------------END OF CHECK-------------------------------------
 
 ##---------------------------FIRST_Q FUCNTION----------------------------------
-function first_q(){
+function questions(){
       $script = $_SERVER['PHP_SELF'];
+
+      session_start();
+
+      if (!isset($_SESSION["question_num"])){
+        $_SESSION["answer"] = "";
+        $_SESSION["score"] = 0;
+        $_SESSION["question_num"] = 1;
+      }
+
+      $total_questions = 6;
+
       print <<<TOP
         <html>
         <head>
@@ -129,29 +124,93 @@ function first_q(){
 TOP;
         $question_num = $_SESSION["question_num"];
 	$answer = $_SESSION["answer"];
+	$score = $_SESSION["score"];
 
-	$answer = "False";
-        $_SESSION["answer"] = $answer;
-
-        $question_num++;
-        $_SESSION["question_num"] = $question_num;
+	if ($question_num == 1){
+  	  $answer = "False";
+          $_SESSION["answer"] = $answer;
+          $question_num++;
+          $_SESSION["question_num"] = $question_num;
 
           print <<<FIRST
           <form method="POST" action="$script">
-          <p> 	<b> 1) According to Kepler the orbit of the earth is a circle 
+          <p>   <b> 1) According to Kepler the orbit of the earth is a circle 
 		       with the sun at the center. </b>
 		<br>
 		<input type="radio" name="TF1" value="True"> True </input>
 		<input type="radio" name="TF1" value="False"> False </input>
 		<br>
-		<input type="submit" name="submit_q1" value="NEXT"/>
+		score $score
+		<br>
+		<input type="submit" name="questions" value="NEXT"/>
+          </p>
+	  </form>
+FIRST;
+	}
+        else if ($question_num == 2){
+          if ($answer == $_POST["TF1"]){
+	     $score += 10;
+	  }
+	  $answer = "True";
+          $question_num++;
+	  $_SESSION["answer"] = $answer;
+          $_SESSION["question_num"] = $question_num;
+	  $_SESSION["score"] = $score;
+
+          print <<<SECOND
+          <form method="POST" action="$script">
+          <p>   <b> 2) Ancient astronomers did consider the heliocentric model 
+		of the solar system but rejected it because they could 
+		not detect parallax. </b>
+		<br>
+                <input type="radio" name="TF2" value="True"> True </input>
+                <input type="radio" name="TF2" value="False"> False </input>
+                <br>
+                score $score
+                <br>
+                <input type="submit" name="questions" value="NEXT"/>
+          </p>
+	  </form>
+SECOND;
+	}
+        else if ($question_num == 3){
+          if ($answer == $_POST["TF2"]){
+             $score += 10;
+          }
+          $answer = "True";
+          $question_num++;
+          $_SESSION["answer"] = $answer;
+          $_SESSION["question_num"] = $question_num;
+          $_SESSION["score"] = $score;
+
+          print <<<THIRD
+          <form method="POST" action="$script">
+          <p>   <b> 3) The total amount of energy that a star emits is directly 
+		related to its </b>
+		<br>
+		<input type="checkbox" name="MC1" value="False"> surface gravity and 
+		magnetic field </input><br>
+		<input type="checkbox" name="MC1" value="True"> radius and temperature 
+		</input><br>
+		<input type="checkbox" name="MC1" value="False"> pressure and volume 
+		</input><br>
+		<input type="checkbox" name="MC1" value="False"> location and velocity 
+		</input>
+		<br>
+                score $score
+                <br>
+                <input type="submit" name="questions" value="NEXT"/>
           </p>
           </form>
+THIRD;
+        }
+
+	print <<<BOTTOM
           </body>
           </html>
-FIRST;
+BOTTOM;
 }
-##--------------------------END OF FIRST_Q-------------------------------------
+##--------------------------END OF QUESTIONS-------------------------------------
 
 
 
