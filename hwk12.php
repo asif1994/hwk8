97ef13f1ba7ce13a372fae8f847002ece9e0f755
asif chowdhury <asif.chowdhury64@gmail.com>
2015-11-21 16:37:40 -0600

    no message

--- hwk12.php
+++ hwk12.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-
+$list = [];
 if (isset($_POST["check"])) {
     check();
 } else if (isset($_POST["questions"])) {
@@ -69,6 +69,7 @@ function check(){
       break;
     }
   }
+  fclose($file);
 
   if ($found){
       $script = $_SERVER['PHP_SELF'];
@@ -84,6 +85,7 @@ function check(){
         <form method="POST" action=$script>
 	  <p> You will be given 6 questions in this quiz. You have 15 minutes to complete this quiz. <br /><br/>
 	      Click below when you are ready to start the quiz. <br>
+	      <input type = "hidden" name = "page" value = $user />
 	      <input type=submit name="questions" value="START"/>
 	  </p>
           </form>
@@ -103,13 +105,14 @@ WELCOME;
 ##---------------------------FIRST_Q FUCNTION----------------------------------
 function questions(){
       $script = $_SERVER['PHP_SELF'];
-
+      $user = $_POST["page"];
       session_start();
 
       if (!isset($_SESSION["question_num"])){
         $_SESSION["answer"] = "";
         $_SESSION["score"] = 0;
         $_SESSION["question_num"] = 1;
+        $_SESSION["start"] = time();
       }
 
       $total_questions = 6;
@@ -120,98 +123,228 @@ function questions(){
         <title> Astronomy Quiz </title>
         </head>
         <body>
-        <h2> TRUE / FALSE </h2>
 TOP;
         $question_num = $_SESSION["question_num"];
 	$answer = $_SESSION["answer"];
 	$score = $_SESSION["score"];
+	$start = $_SESSION["start"];
 
-	if ($question_num == 1){
-  	  $answer = "False";
-          $_SESSION["answer"] = $answer;
-          $question_num++;
-          $_SESSION["question_num"] = $question_num;
+	if (time() - $start >= 30){
+	  print <<<NOTIME
+          <h2> Sorry, Out of Time </h2>
+          <p>
+          <b> Sorry! </b>
+          You're out of time.
+          <br>
+          Your score is $score.
+          <br>`
+          </p>
+NOTIME;
+        $result = $user . ":" . $score . "\n";
+        $file = fopen("./results.txt", "a");
+        fwrite($file, "$result");
+        fclose($file);
 
-          print <<<FIRST
-          <form method="POST" action="$script">
-          <p>   <b> 1) According to Kepler the orbit of the earth is a circle 
-		       with the sun at the center. </b>
-		<br>
-		<input type="radio" name="TF1" value="True"> True </input>
+        session_destroy();
+	}
+	else {
+	  if ($question_num == 1){
+	    $status = "inactive";
+	    $_SESSION['status'] = $status;
+
+            $question_num++;
+            $_SESSION["question_num"] = $question_num;
+	    print_r ($GLOBALS['status']);
+            print <<<FIRST
+	    <h2> TRUE / FALSE </h2>
+            <form method="POST" action="$script">
+            <p> <b> 1) According to Kepler the orbit of the earth is a circle 
+	  	       with the sun at the center. </b>
+	  	<br>
+		<input type="radio" name="TF2" value="True"> True </input>
 		<input type="radio" name="TF1" value="False"> False </input>
 		<br>
 		score $score
 		<br>
+		<input type = "hidden" name = "page" value = $user />
 		<input type="submit" name="questions" value="NEXT"/>
-          </p>
-	  </form>
+            </p>
+	    </form>
 FIRST;
+            $status = "active";
+            $_SESSION['status'] = $status;
 	}
         else if ($question_num == 2){
-          if ($answer == $_POST["TF1"]){
+          if (isset($_POST["TF1"])){
 	     $score += 10;
 	  }
-	  $answer = "True";
-          $question_num++;
-	  $_SESSION["answer"] = $answer;
-          $_SESSION["question_num"] = $question_num;
+	  $question_num++;
+	  $_SESSION["question_num"] = $question_num;
 	  $_SESSION["score"] = $score;
 
           print <<<SECOND
+	  <h2> TRUE / FALSE </h2>
           <form method="POST" action="$script">
           <p>   <b> 2) Ancient astronomers did consider the heliocentric model 
 		of the solar system but rejected it because they could 
 		not detect parallax. </b>
 		<br>
                 <input type="radio" name="TF2" value="True"> True </input>
-                <input type="radio" name="TF2" value="False"> False </input>
+                <input type="radio" name="TF1" value="False"> False </input>
                 <br>
-                score $score
+                Score: $score
                 <br>
+		<input type = "hidden" name = "page" value = $user />
                 <input type="submit" name="questions" value="NEXT"/>
           </p>
 	  </form>
 SECOND;
 	}
         else if ($question_num == 3){
-          if ($answer == $_POST["TF2"]){
+          if (isset($_POST["TF2"])){
              $score += 10;
           }
-          $answer = "True";
           $question_num++;
-          $_SESSION["answer"] = $answer;
           $_SESSION["question_num"] = $question_num;
           $_SESSION["score"] = $score;
 
           print <<<THIRD
+	  <h2> Multiple Choice </h2>
           <form method="POST" action="$script">
           <p>   <b> 3) The total amount of energy that a star emits is directly 
 		related to its </b>
 		<br>
-		<input type="checkbox" name="MC1" value="False"> surface gravity and 
+		<input type="checkbox" name="MC_1" value="False"> surface gravity and
 		magnetic field </input><br>
-		<input type="checkbox" name="MC1" value="True"> radius and temperature 
+		<input type="checkbox" name="MC_2" value="True"> radius and 
+		temperature </input><br>
+		<input type="checkbox" name="MC_3" value="False"> pressure and volume
 		</input><br>
-		<input type="checkbox" name="MC1" value="False"> pressure and volume 
-		</input><br>
-		<input type="checkbox" name="MC1" value="False"> location and velocity 
-		</input>
+		<input type="checkbox" name="MC_4" value="False"> location and 
+		velocity </input>
 		<br>
-                score $score
+                Score: $score
                 <br>
+		<input type = "hidden" name = "page" value = $user />
                 <input type="submit" name="questions" value="NEXT"/>
           </p>
           </form>
 THIRD;
         }
+	else if ($question_num == 4) {
+	   if ((isset($_POST["MC_2"])) && !(isset($_POST["MC_1"])) && !(isset($_POST["MC_3"])) && !(isset($_POST["MC_4"])))
+	   {
+	       $score += 10;
+	   }
+          $question_num++;
+          $_SESSION["question_num"] = $question_num;
+          $_SESSION["score"] = $score;
 
+
+          print <<<FOURTH
+ 	  <h2> Multiple Choice </h2>
+          <form method="POST" action="$script">
+          <p>
+	  <b> 4) Stars that live the longest have </b>
+  	  <br>
+	  <input type="checkbox" name="MC_1" value="False"> high mass </input><br>
+	  <input type="checkbox" name="MC_2" value="False"> high temperature </input><br>
+	  <input type="checkbox" name="MC_3" value="False"> lots of hydrogen </input><br>
+	  <input type="checkbox" name="MC_4" value="True"> small mass </input>
+	  <br>
+	  Score: $score      
+	  <br>
+	  <input type = "hidden" name = "page" value = $user />
+          <input type="submit" name="questions" value="NEXT"/>
+          </p>
+          </form>
+FOURTH;
+	}
+
+	else if ($question_num == 5){
+           if ((isset($_POST["MC_4"])) && !(isset($_POST["MC_1"])) && !(isset($_POST["MC_2"])) && !(isset($_POST["MC_3"])))
+           {
+               $score += 10;
+           }
+	  $answer = "galaxy";
+          $question_num++;
+          $_SESSION["question_num"] = $question_num;
+          $_SESSION["score"] = $score;
+	  $_SESSION["answer"] = $answer;
+
+          print <<<FIFTH
+          <h2> Fill in the Blank </h2>
+          <form method="POST" action="$script">
+	  <b> 5) A collection of a hundred billion stars, gas, and dust is called a
+		<input type="text" name="FB1" value="" id="Q5"/>. </b>
+	  <br>
+          Score: $score
+	  <br>
+	  <input type = "hidden" name = "page" value = $user />
+          <input type="submit" name="questions" value="NEXT"/>
+          </p>
+	  </form>
+FIFTH;
+        }
+
+        else if ($question_num == 6){
+	   $data = trim($_POST["FB1"]);
+	   $data = strtolower($data);
+           if ($answer == $data)
+           {
+               $score += 10;
+           }
+          $answer = "age";
+          $question_num++;
+          $_SESSION["question_num"] = $question_num;
+          $_SESSION["score"] = $score;
+          $_SESSION["answer"] = $answer;
+
+          print <<<SIXTH
+          <h2> Fill in the Blank </h2>
+          <form method="POST" action="$script">
+          <b> 6) The inverse of the Hubble's constant is a measure of the 
+	<input type="text" name="FB2" value="" id="Q6"/> of the universe.</b>
+          <br>
+          Score: $score
+          <br>
+	  <input type = "hidden" name = "page" value = $user />
+          <input type="submit" name="questions" value="SUBMIT"/>
+          </p>
+          </form>
+SIXTH;
+        }
+        else if ($question_num > 6){
+           $data = trim($_POST["FB2"]);
+           $data = strtolower($data);
+           if ($answer == $data)
+           {
+               $score += 10;
+           }
+          print <<<LAST
+          <h2> Thank you for Taking the Quiz </h2>
+          <p>
+          <b> Congrations! </b>
+	  You've completed this quiz.
+          <br>
+          Your score is $score.
+          <br>`
+          </p>
+LAST;
+	$result = $user . ":" . $score . "\n";
+	$file = fopen("./results.txt", "a");
+	fwrite($file, "$result");
+        fclose($file);
+
+	session_destroy(); 
+	}
+
+}
 	print <<<BOTTOM
           </body>
           </html>
 BOTTOM;
+
 }
 ##--------------------------END OF QUESTIONS-------------------------------------
-
-
-
 ?>
